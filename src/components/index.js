c3afec7cb8993997c28477eb994bf50e980d9fb8
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2021-01-19 21:47:39 +0530

    Updated Code

--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,73 @@
 //      children: []
 //}
 
-function updateStructure(rec1,rec2){
-	//write your code
+function updateStructure(recA, recB) {
+	//contains(A, B) returns true/false if B contained in A
+	//relative(A, B) returns relative dimensions of B with respect with A
+	if(contains(recA, recB)) {
+		const relativeDim = relative(recA, recB);
+		return {...recA, children: [relativeDim]};
+	} else if(contains(recB, recA)) {
+		return {...recB, children: [relativeDim]};
+	} else {
+		return {...recA};
+	}
+}
+
+function contains(recA, recB) {
+	const recAn = normalize(recA);
+	const recBn = normalize(recB);
+
+	if(
+		recAn.x1 <= recBn.x1 
+		&& recAn.y1 <= recBn.y1 
+		&& recAn.x2 >= recBn.x2 
+		&& recAn.y2 >= recBn.y2
+	) {
+		return true;
+	}
+	return false;
+}
+
+function relative(recA, recB) {
+	const recAn = normalize(recA);
+	const recBn = normalize(recB);
+
+	const res = {
+		children: recB.children
+	}
+
+	if(recB.top) {
+		res.top = `${recBn.x1 - recAn.x1}px`;
+	}
+	if(recB.left) {
+		res.left = `${recBn.y1 - recAn.y1}px`;
+	}
+	if(recB.height) {
+		res.height = recB.height;
+	}
+	if(recB.width) {
+		res.width = recB.width;
+	}
+	if(recB.bottom) {
+		res.bottom = `${recAn.x2 - recBn.x2}px`;
+	}
+	if(recB.right) {
+		res.right = `${recAn.y2 - recBn.y2}px`;
+	}
+	return res;
+}
+
+const T = 0; //total height
+const W = 0; //total width
+
+function normalize(rec) {
+	return {
+		x1: rec.top ? parseInt(rec.top) : (T - (parseInt(rec.bottom) + parseInt(rec.height))),
+		y1: rec.left ? parseInt(rec.left) : (W - (parseInt(rec.right) + parseInt(rec.width))),
+		x2: rec.bottom ? (T - parseInt(rec.bottom)) : parseInt(rec.top) + parseInt(rec.height),
+		y2: rec.right ? (W - parseInt(rec.right)) : parseInt(rec.left) + parseInt(rec.width)
+	}
 }
 
 module.exports = updateStructure;
